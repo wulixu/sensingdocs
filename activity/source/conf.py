2c8df4d9a720df98598b3bbaa66b3fd272aaea94
wulixu.william@hotmail.com <wulixu.william@hotmail.com>
2016-03-24 20:30:17 +0800

    remove the tst

--- activity/source/conf.py
+++ activity/source/conf.py
@@ -233,24 +233,6 @@ htmlhelp_basename = 'SensingGamedoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-
-# Latex figure (float) alignment
-#'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
 #latex_logo = None
